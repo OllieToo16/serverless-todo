a6c4956d48ab62f89c4f3042108343ee18cc02f5
OllieToo16 <cheruiyotko98@gmail.com>
2022-08-28 18:31:28 +0300

    Changes Commit

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,39 +1,3 @@
-// import 'source-map-support/register'
-
-// import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-// // import * as middy from 'middy'
-// // import { cors } from 'middy/middlewares'
-// import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-// import { createTodo } from '../../businessLogic/todos'
-
-// export const handler = middy(
-//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//     console.log(`Processing event ${event}`)
-//     const newTodo: CreateTodoRequest = JSON.parse(event.body)
-//     // TODO: Implement creating a new TODO item
-//     const authorization = event.headers.Authorization
-//     const split = authorization.split(' ')
-//     const jwtToken = split[1]
-
-    
-//       const todoItem = await createTodo(newTodo, jwtToken)
-
-//     return {
-//       statusCode: 201,
-//       body: JSON.stringify({
-//         'items': todoItem
-//       })
-//     }
-//   }
-// )
-
-// handler.use(
-//   cors({
-//     credentials: true
-//   })
-// )
-
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
 // import * as middy from 'middy'
--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,39 +1,3 @@
-// import 'source-map-support/register'
-
-// import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-// import * as middy from 'middy'
-// import { cors, httpErrorHandler } from 'middy/middlewares'
-
-// import { deleteTodo } from '../../businessLogic/todos'
-
-// export const handler = middy(
-//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//     const todoId = event.pathParameters.todoId
-//     // TODO: Remove a TODO item by id
-    
-//     const authorization = event.headers.Authorization;
-//     const split = authorization.split(' ');
-//     const jwtToken = split[1];
-  
-//     const deletedTodo = await deleteTodo(todoId, jwtToken);
-  
-//     return {
-//       statusCode: 200,
-//       body: deletedTodo,
-//     }
-  
-//     }
-//   )
-
-// handler
-//   .use(httpErrorHandler())
-//   .use(
-//     cors({
-//       credentials: true
-//     })
-//   )
-
-
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
